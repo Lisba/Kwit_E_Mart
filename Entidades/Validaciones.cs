20e06d85ec858ab87161690f0ab055c1986b13e5
Lisbaldy <lisba7474@gmail.com>
2020-09-27 00:19:41 -0300

    Validaciones agregadas

--- Entidades/Validaciones.cs
+++ Entidades/Validaciones.cs
@@ -9,10 +9,32 @@ namespace Entidades
     static class Validaciones
     {
         #region Methods
+        /// <summary>
+        /// Valida que el string ingresado sea un entero válido.
+        /// </summary>
+        /// <param name="strInt"></param>
+        /// <returns>Retorna el numero entero ó -1 en caso de no ser un entero.</returns>
+        static public int ValidarInt(string strInt)
+        {
+            bool isInt;
+            int intReturned;
+
+            isInt = int.TryParse(strInt, out intReturned);
+
+            if (isInt)
+            {
+                return intReturned;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
         static public double ValidarDouble(string strDouble)
         {
             bool isDouble = true;
-            double doubleReturned = 0;
+            double doubleReturned = -1;
             int comaCounter = 0;
 
             foreach (char character in strDouble)
@@ -36,7 +58,7 @@ namespace Entidades
 
             if (isDouble)
             {
-                Double.TryParse(strDouble, out doubleReturned);
+                double.TryParse(strDouble, out doubleReturned);
             }
 
             return doubleReturned;
